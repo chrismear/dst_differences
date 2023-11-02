88e200d3bd7ee7ba84aa8c5a2c6180f848340f81
Chris Tucker Mear <chris@feedmechocolate.com>
2023-11-02 15:11:45 +0000

    Report years in the future

--- dst_differences.rb
+++ dst_differences.rb
@@ -11,25 +11,28 @@ zonename2 = ARGV[1]
 tz1 = TZInfo::Timezone.get(zonename1)
 tz2 = TZInfo::Timezone.get(zonename2)
 
-dst_entrances1 = tz1.transitions_up_to(Time.now).select { |transition|
+# Five years after today
+end_time = Time.now.to_time + 5*365*24*60*60
+
+dst_entrances1 = tz1.transitions_up_to(end_time).select { |transition|
   transition.offset.std_offset == 3600 &&
     transition.previous_offset.std_offset == 0
 }
 dst_entrance_days1 = dst_entrances1.map { |transition| transition.local_end_at.to_time }
 
-dst_entrances2 = tz2.transitions_up_to(Time.now).select { |transition|
+dst_entrances2 = tz2.transitions_up_to(end_time).select { |transition|
   transition.offset.std_offset == 3600 &&
     transition.previous_offset.std_offset == 0
 }
 dst_entrance_days2 = dst_entrances2.map { |transition| transition.local_end_at.to_time }
 
-dst_exits1 = tz1.transitions_up_to(Time.now).select { |transition|
+dst_exits1 = tz1.transitions_up_to(end_time).select { |transition|
   transition.offset.std_offset == 0 &&
     transition.previous_offset.std_offset == 3600
 }
 dst_exit_days1 = dst_exits1.map { |transition| transition.local_end_at.to_time }
 
-dst_exits2 = tz2.transitions_up_to(Time.now).select { |transition|
+dst_exits2 = tz2.transitions_up_to(end_time).select { |transition|
   transition.offset.std_offset == 0 &&
     transition.previous_offset.std_offset == 3600
 }
@@ -50,11 +53,11 @@ output.push ["year", "#{zonename1} start", "#{zonename2} start", "difference in 
 earliest_year.upto(latest_year) do |year|
   entrance1 = dst_entrance_days1.select{ |time| time.year == year}[0]
   entrance2 = dst_entrance_days2.select{ |time| time.year == year}[0]
-  
+
   exit1 = dst_exit_days1.select{ |time| time.year == year}[0]
   exit2 = dst_exit_days2.select{ |time| time.year == year}[0]
   next unless entrance1 && entrance2 && exit1 && exit2
-  
+
   output.push [
     year,
     entrance1.strftime("%d %B"), entrance2.strftime("%d %B"), ((entrance2 - entrance1)/(24*60*60)).to_i,
